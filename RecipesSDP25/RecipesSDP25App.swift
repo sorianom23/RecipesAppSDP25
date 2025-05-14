10bc0e363db97616b21169a7cf25df09a95f7189
sorianom23 <soriano.mariacl@gmail.com>
2025-05-14 09:56:08 +0300

    Initial Commit

---
+++ RecipesSDP25/RecipesSDP25App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesSDP25App.swift
+//  RecipesSDP25
+//
+//  Created by Maria Soriano on 14/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesSDP25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
